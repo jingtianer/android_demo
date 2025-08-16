5b82155e0325d2a89adf90a962cc26958db197c4
jingtian.liu <jingtian.liu@bytedance.com>
2025-08-17 03:30:42 +0800

    bitmap缓存画布

--- app/src/main/java/com/jingtian/demoapp/main/widget/TextOnFingerView.kt
+++ app/src/main/java/com/jingtian/demoapp/main/widget/TextOnFingerView.kt
@@ -1,12 +1,14 @@
 package com.jingtian.demoapp.main.widget
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.PathMeasure
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.ColorInt
@@ -21,7 +23,7 @@ class TextOnFingerView @JvmOverloads constructor(
     private val paint = Paint()
     private var textColor = Color.RED
     private var path = Path()
-    private var text = "Hello World!"
+    private var text = "HelloWorld!"
 
     private var textWidth = ArrayList<Lazy<Float>>(text.length)
 
@@ -37,6 +39,18 @@ class TextOnFingerView @JvmOverloads constructor(
     private var y = 0f
     private var drawInfo = DrawInfo()
 
+    private val bitMap by lazy {
+        Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888)
+    }
+    private val cacheCanvas by lazy {
+        Canvas(bitMap)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        bitMap.reconfigure(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888)
+    }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         x = event.x
         y = event.y
@@ -48,11 +62,12 @@ class TextOnFingerView @JvmOverloads constructor(
                 drawInfo = DrawInfo()
             }
             MotionEvent.ACTION_MOVE -> {
-                path.quadTo(mLastX, mLastY, (x + mLastX) / 2, (y + mLastY) / 2)
+                path.lineTo(x, y)
                 drawInfo.cal()
                 invalidate()
             }
-            MotionEvent.ACTION_UP -> {
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                cacheCanvas.drawCurrentData()
             }
         }
         mLastX = x
@@ -97,22 +112,35 @@ class TextOnFingerView @JvmOverloads constructor(
         drawInfo.cal()
     }
 
-    inner class DrawInfo {
+    private fun String.rotate(N: Int): String {
+        return substring(N, length) + substring(0, N)
+    }
+
+    inner class DrawInfo(private val offset: Int = 0) {
         private var repeatedString = ""
         private var currentRepeatCnt = 0
-        var drawText = ""
+        private var remainText = 0
+        private val rotatedText = text.rotate(offset)
+        private var doubleText = rotatedText + rotatedText
+        val drawText: String
+            get() {
+                if (offset > repeatedString.length) {
+                    return doubleText.substring(0, remainText)
+                }
+                return repeatedString + doubleText.substring(0, remainText)
+            }
 
         fun cal() {
-            if (text.isEmpty()) {
+            if (rotatedText.isEmpty()) {
                 return
             }
             val pathMeasure = PathMeasure(path, false)
             val pathLength = pathMeasure.measure()
             val repeatCnt = (pathLength / textWidth.last().value).toInt()
-            repeatedString += text.repeat(repeatCnt - currentRepeatCnt)
+            repeatedString += rotatedText.repeat(repeatCnt - currentRepeatCnt)
             currentRepeatCnt = repeatCnt
             val remainedLength = pathLength - repeatCnt * textWidth.last().value
-            var remainText = textWidth.binarySearch {
+            remainText = textWidth.binarySearch {
                 if (it.value == remainedLength) {
                     0
                 } else if (it.value > remainedLength) {
@@ -122,10 +150,15 @@ class TextOnFingerView @JvmOverloads constructor(
                 }
             }
             if (remainText < 0) {
-                remainText = -remainText - 2
+                remainText = -remainText - 1
+            } else if (remainText < text.length){
+                remainText + 1
             }
-            if (remainText > 0) {
-                drawText = repeatedString + text.substring(0, remainText)
+            if (repeatCnt >= 10) {
+                cacheCanvas.drawCurrentData()
+                path.reset()
+                path.moveTo(x, y)
+                drawInfo = DrawInfo((remainText + offset) % text.length)
             }
         }
     }
@@ -141,12 +174,13 @@ class TextOnFingerView @JvmOverloads constructor(
         return totalLength
     }
 
+    private fun Canvas.drawCurrentData() {
+        drawTextOnPath(drawInfo.drawText, path, 0f, 0f, paint)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if (text.isEmpty()) {
-            return
-        }
-        canvas.drawTextOnPath(drawInfo.drawText, path, 0f, 0f, paint)
-        path
+        canvas.drawBitmap(bitMap, 0f, 0f, null)
+        canvas.drawCurrentData()
     }
 }
