496bc6ddcc48c35dd5a31a38e06e0cf824075f24
jingtian.liu <jingtian.liu@bytedance.com>
2026-02-14 11:02:47 +0800

    BasicText点击事件

--- composedemo/src/main/java/com/jingtian/composedemo/MainActivity.kt
+++ composedemo/src/main/java/com/jingtian/composedemo/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.jingtian.composedemo
 
-import androidx.collection.mutableObjectListOf
 import androidx.collection.objectListOf
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.jingtian.composedemo.base.AppThemeText
-import com.jingtian.composedemo.base.AppThemeTextField
 import com.jingtian.composedemo.base.BaseActivity
 
 class MainActivity : BaseActivity() {
@@ -25,6 +16,7 @@ class MainActivity : BaseActivity() {
 
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun Main() {
