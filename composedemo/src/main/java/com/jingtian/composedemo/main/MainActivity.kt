8584a7219cc73fdd503b1c41df0bea8f6789e0ff
jingtian.liu <1043672911@qq.com>
2026-03-04 18:41:38 +0800

    drawer, windowInsets

--- composedemo/src/main/java/com/jingtian/composedemo/main/MainActivity.kt
+++ composedemo/src/main/java/com/jingtian/composedemo/main/MainActivity.kt
@@ -18,9 +18,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -164,7 +166,8 @@ fun Main() {
     ) { _->
         ModalNavigationDrawer(
             drawerContent = {
-                ModalDrawerSheet(drawerContainerColor = LocalAppPalette.current.drawerBg, windowInsets = WindowInsets.navigationBars) {
+                ModalDrawerSheet(drawerContainerColor = LocalAppPalette.current.drawerBg, windowInsets = WindowInsets.navigationBars.only(
+                    WindowInsetsSides.Vertical + WindowInsetsSides.Start)) {
                     MainDrawer(menuItemsEntity) { index, album ->
                         currentSelectedAlbum = IndexedValue(index, album)
                         scope.launch {
