e013c32d45bb97d212953aff8b80246f0c20b48f
jingtian.liu <1043672911@qq.com>
2026-03-11 14:06:54 +0800

    存储窗口状态

--- composedemo/src/desktopMain/kotlin/com/jingtian/composedemo/ui/theme/Theme.kt
+++ composedemo/src/desktopMain/kotlin/com/jingtian/composedemo/ui/theme/Theme.kt
@@ -1,25 +1,15 @@
 package com.jingtian.composedemo.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.jingtian.composedemo.utils.AppTheme
 import com.jingtian.composedemo.viewmodels.AppThemeViewModel
 
 
@@ -32,8 +22,8 @@ actual fun Modifier.appBackground(heightScale: Float): Modifier {
 }
 
 data class DesktopConst(
-    val screenWidthDp: Dp = 800.dp,
-    val screenHeightDp: Dp = 600.dp,
+    val screenWidthDp: Dp = 0.dp,
+    val screenHeightDp: Dp = 0.dp,
 )
 
 val LocalDesktopConst = compositionLocalOf(structuralEqualityPolicy()) { DesktopConst() }
