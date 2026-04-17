e36fb372554107066ce0edb393acb95dfe32938a
jingtian.liu <1043672911@qq.com>
2026-04-17 16:52:26 +0800

    ios 图标 weakRef

--- composedemo/src/iosMain/kotlin/com.jingtian.composedemo/multiplatform/WeakRef.kt
+++ composedemo/src/iosMain/kotlin/com.jingtian.composedemo/multiplatform/WeakRef.kt
@@ -1,9 +1,12 @@
 package com.jingtian.composedemo.multiplatform
 
 import androidx.compose.ui.node.WeakReference
+import kotlin.experimental.ExperimentalNativeApi
 
 actual class WeakRef<T : Any> actual constructor(value: T) {
-    private val ref = WeakReference(value)
+    @OptIn(ExperimentalNativeApi::class)
+    private val ref = kotlin.native.ref.WeakReference(value)
+    @OptIn(ExperimentalNativeApi::class)
     actual fun get(): T? {
         return ref.get()
     }
